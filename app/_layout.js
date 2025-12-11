984457a7f9a10594424158b22226088c6355f4a7
mkadri66 <mustapha.kadri66@gmail.com>
2025-12-11 12:30:10 +0100

    init backend with prisma

---
+++ app/_layout.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = RootLayout;
+const expo_router_1 = require("expo-router");
+function RootLayout() {
+    return <expo_router_1.Stack screenOptions={{
+            headerShown: false
+        }}/>;
+}
